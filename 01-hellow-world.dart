49fafdc892d92c9f5152be16f21298a3dbc717fe
JesusxAguirre <99jesus.aguirre@gmail.com>
2025-03-11 17:21:02 -0400

    upload flutter projects in local

---
+++ 01-hellow-world.dart
@@ -0,0 +1,9 @@
+void main() {
+  // var myName = 'Jesus';
+
+  String myName = 'Jesus';
+
+  // final myName = 'Jesus';
+
+  print('Hello, $myName!');
+}
---
+++ 02-variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  final String pokemon = 'Arcanine';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> habilities = ['flamethrower', 'ember'];
+
+  print("""
+    $pokemon
+    $hp
+    $isAlive
+    $habilities
+  """);
+}
